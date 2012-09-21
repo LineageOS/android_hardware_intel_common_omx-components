9101b0d1ca310418db57a1a394f5990e717df647
wfeng6 <wei.feng@intel.com>
2012-09-21 18:51:51 +0800

    Remove unused codes and config files in video middleware stack
    BZ: 58358
    
    Remove unused codes and config files in video middleware stack.
    
    Change-Id: Ida0fd634e44465ad8c9f9eadc49cee82cd127b0b
    Signed-off-by: wfeng6 <wei.feng@intel.com>
    Reviewed-on: http://android.intel.com:8080/67296
    Reviewed-by: Xiao, FengX <fengx.xiao@intel.com>
    Reviewed-by: Jiang, Fei <fei.jiang@intel.com>
    Reviewed-by: Liu, BolunX <bolunx.liu@intel.com>
    Reviewed-by: Zhao, Leo <leo.zhao@intel.com>
    Reviewed-by: Zeng, Li <li.zeng@intel.com>
    Reviewed-by: Guo, Nana N <nana.n.guo@intel.com>
    Reviewed-by: Tang, Richard <richard.tang@intel.com>
    Reviewed-by: Qiu, Junhai <junhai.qiu@intel.com>
    Reviewed-by: Chen, Tianmi <tianmi.chen@intel.com>
    Reviewed-by: Gu, Wangyi <wangyi.gu@intel.com>
    Reviewed-by: Sun, Mingruo <mingruo.sun@intel.com>
    Reviewed-by: Shi, PingX <pingx.shi@intel.com>
    Tested-by: Shi, PingX <pingx.shi@intel.com>
    Reviewed-by: buildbot <buildbot@intel.com>
    Tested-by: buildbot <buildbot@intel.com>

--- Android.mk
+++ Android.mk
@@ -5,22 +5,7 @@ include $(CLEAR_VARS)
 
 VENDORS_INTEL_MRST_COMPONENTS_ROOT := $(LOCAL_PATH)
 
-$(info WARNING: PRODUCT_COPY_FILES disabled in $(LOCAL_PATH))
-#PRODUCT_COPY_FILES += \
-#	$(LOCAL_PATH)/wrs_omxil_components.list:system/etc/wrs_omxil_components.list
-#$(call add-prebuilt-files, ETC, wrs_omxil_components.list)
-
-# mrst sst audio
-#-include $(VENDORS_INTEL_MRST_COMPONENTS_ROOT)/sst/Android.mk
-
 #intel video codecs
-include $(VENDORS_INTEL_MRST_COMPONENTS_ROOT)/psb-dec/Android.mk
-include $(VENDORS_INTEL_MRST_COMPONENTS_ROOT)/avc-enc/Android.mk
-include $(VENDORS_INTEL_MRST_COMPONENTS_ROOT)/h263-enc/Android.mk
-include $(VENDORS_INTEL_MRST_COMPONENTS_ROOT)/m4v-enc/Android.mk
 include $(VENDORS_INTEL_MRST_COMPONENTS_ROOT)/videocodec/Android.mk
-
-#intel audio codecs
-#-include $(VENDORS_INTEL_MRST_COMPONENTS_ROOT)/sst-stub-base/Android.mk
 
 endif #BOARD_USES_MRST_OMX
--- ebuild.mk
+++ ebuild.mk
@@ -3,5 +3,3 @@ include $(CLEAR_VARS)
 
 VENDORS_INTEL_MRST_COMPONENTS_ROOT := $(LOCAL_PATH)
 
-# mrst sst audio
--include $(VENDORS_INTEL_MRST_COMPONENTS_ROOT)/sst/ebuild.mk
