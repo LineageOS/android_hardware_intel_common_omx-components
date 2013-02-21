4b4fefee98b3224fbd3a88b150db3d253997fdf8
Guilhem IMBERTON <guilhem.imberton@intel.com>
2013-02-21 09:29:02 +0100

    REF_DEVICE_NAME from ctp_pr1 to redhookbay
    BZ: 88279
    
    Finishing name change in FT Makefiles.
    Urgent for TW external release.
    
    Change-Id: Ice1d4ea4f329ba6ad9d169f3ae341e67b3a0b283
    Signed-off-by: Guilhem IMBERTON <guilhem.imberton@intel.com>
    Reviewed-on: http://android.intel.com:8080/92978
    Reviewed-by: Chotard, Celine <celine.chotard@intel.com>
    Tested-by: Chotard, Celine <celine.chotard@intel.com>
    Reviewed-by: buildbot <buildbot@intel.com>
    Tested-by: buildbot <buildbot@intel.com>

--- videocodec/Android.mk
+++ videocodec/Android.mk
@@ -1,15 +1,15 @@
 ifeq ($(strip $(BOARD_USES_WRS_OMXIL_CORE)),true)
 LOCAL_PATH := $(call my-dir)
 
-ifeq ($(TARGET_DEVICE),mfld_gi)
+ifeq ($(REF_PRODUCT_NAME),mfld_gi)
 LOCAL_C_FLAGS := -DMFLD_GI
-else ifneq (,$(findstring $(TARGET_DEVICE),mfld_dv10 redridge salitpa))
+else ifneq (,$(findstring $(REF_PRODUCT_NAME),salitpa))
 LOCAL_C_FLAGS := -DMFLD_DV10
-else ifneq (,$(findstring $(TARGET_DEVICE),victoriabay ctp_pr1 ctp_nomodem))
+else ifneq (,$(findstring $(REF_PRODUCT_NAME),victoriabay redhookbay))
 LOCAL_C_FLAGS := -DCLVT
-else ifeq ($(TARGET_DEVICE), mrfl_vp)
+else ifeq ($(REF_PRODUCT_NAME), mrfl_vp)
 LOCAL_C_FLAGS := -DMRFL_VP
-else ifeq ($(TARGET_DEVICE),yukkabeach)
+else ifeq ($(REF_PRODUCT_NAME),yukkabeach)
 LOCAL_C_FLAGS := -DYUKKA
 else
 LOCAL_C_FLAGS := -DMFLD_PR2
