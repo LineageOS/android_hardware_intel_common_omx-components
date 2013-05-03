066e20bf6635dac50a515a0729acbe3139426973
liu, Bolun <bolun.liu@intel.com>
2013-05-03 09:24:48 +0800

    Re-calculate OMX Output Buffer Size.
    BZ: 104296
    
    Re-calculate OMX Output Buffer Size as Widh*Height*1.5/2.
    It is just work around method to fix output buffer too small issue.
    Rate control problem root caused this issue.
    
    Change-Id: I1b8f82181ddbfdadb27792d46935889dcf18054c
    Signed-off-by: liu, Bolun <bolun.liu@intel.com>
    Reviewed-on: http://android.intel.com:8080/105909
    Reviewed-by: cactus <cactus@intel.com>
    Reviewed-by: Zhao, Leo <leo.zhao@intel.com>
    Reviewed-by: Yuan, Shengquan <shengquan.yuan@intel.com>
    Reviewed-by: Shi, PingX <pingx.shi@intel.com>
    Tested-by: Shi, PingX <pingx.shi@intel.com>
    Reviewed-by: buildbot <buildbot@intel.com>
    Tested-by: buildbot <buildbot@intel.com>

--- videocodec/OMXVideoEncoderBase.cpp
+++ videocodec/OMXVideoEncoderBase.cpp
@@ -865,8 +865,10 @@ OMX_ERRORTYPE OMXVideoEncoderBase::SetStoreMetaDataInBuffers(OMX_PTR pStructure)
 
     paramPortDefinitionOutput_get = output_port->GetPortDefinition();
     paramPortDefinitionOutput = (OMX_PARAM_PORTDEFINITIONTYPE *)paramPortDefinitionOutput_get;
-    maxSize = mParamBitrate.nTargetBitrate/8;
-    maxSize = min(maxSize, paramPortDefinitionOutput->format.video.nFrameHeight*paramPortDefinitionOutput->format.video.nFrameWidth*1.5/2);
+//  Fix me: just work around. Need tuning bitrate performance.
+//  maxSize = mParamBitrate.nTargetBitrate/8;
+//  maxSize = min(maxSize, paramPortDefinitionOutput->format.video.nFrameHeight*paramPortDefinitionOutput->format.video.nFrameWidth*1.5/2);
+    maxSize = paramPortDefinitionOutput->format.video.nFrameHeight*paramPortDefinitionOutput->format.video.nFrameWidth*1.5/2;
     paramPortDefinitionOutput->nBufferSize =  maxSize;
     LOGD("overwrite output port buffer paramPortDefinitionOutput->nBufferSize is %d",paramPortDefinitionOutput->nBufferSize);
 
