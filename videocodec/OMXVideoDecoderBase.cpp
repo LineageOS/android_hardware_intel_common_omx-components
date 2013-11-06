65c861a00bba638df5ba214b25605f0390a53bfe
Dan Liang <dan.liang@intel.com>
2013-11-06 10:28:38 +0800

    omx-component: use pOutputPortPrivate instead to report error
    BZ: 147912
    
    use pOutputPortPrivate instead of pAppPrivate.
    
    Change-Id: I153edc9c6bbacbe16125d790fcc676008dffb33a
    Signed-off-by: Dan Liang <dan.liang@intel.com>

--- videocodec/OMXVideoDecoderBase.cpp
+++ videocodec/OMXVideoDecoderBase.cpp
@@ -459,9 +459,6 @@ OMX_ERRORTYPE OMXVideoDecoderBase::PrepareConfigBuffer(VideoConfigBuffer *p) {
 
     }
 
-    if (mErrorReportEnabled)
-        p->flag |= WANT_ERROR_REPORT;
-
     p->rotationDegrees = mRotationDegrees;
 #ifdef TARGET_HAS_VPP
     p->vppBufferNum = mVppBufferNum;
@@ -532,10 +529,10 @@ OMX_ERRORTYPE OMXVideoDecoderBase::FillRenderBuffer(OMX_BUFFERHEADERTYPE **pBuff
     }
 
     if (mWorkingMode == GRAPHICBUFFER_MODE && mErrorReportEnabled) {
-        if (buffer->pAppPrivate == NULL)
+        if (buffer->pOutputPortPrivate == NULL)
             LOGE("The App doesn't provide the output buffer for error reporting");
         else
-            ErrBufPtr = (VideoErrorBuffer *)buffer->pAppPrivate;
+            ErrBufPtr = (VideoErrorBuffer *)buffer->pOutputPortPrivate;
     }
 
     bool draining = (inportBufferFlags & OMX_BUFFERFLAG_EOS);
@@ -923,16 +920,17 @@ OMX_ERRORTYPE OMXVideoDecoderBase::GetErrorReportMode(OMX_PTR pStructure) {
 }
 
 OMX_ERRORTYPE OMXVideoDecoderBase::SetErrorReportMode(OMX_PTR pStructure) {
-    CHECK_SET_PARAM_STATE();
+    OMX_ERRORTYPE ret;
 
-    if (pStructure) {
-        mErrorReportEnabled = *(static_cast<bool *>(pStructure));
-        LOGD("Error reporting is %s", mErrorReportEnabled ? "enabled" : "disabled");
+    OMX_VIDEO_CONFIG_INTEL_ERROR_REPORT *p = (OMX_VIDEO_CONFIG_INTEL_ERROR_REPORT *)pStructure;
+    CHECK_TYPE_HEADER(p);
+    CHECK_PORT_INDEX(p, OUTPORT_INDEX);
 
-        return OMX_ErrorNone;
-    } else {
-        return OMX_ErrorBadParameter;
-    }
+    mErrorReportEnabled = p->bEnable;
+    LOGD("Error reporting is %s", mErrorReportEnabled ? "enabled" : "disabled");
+
+    mVideoDecoder->enableErrorReport(mErrorReportEnabled);
+    return OMX_ErrorNone;
 }
 
 OMX_COLOR_FORMATTYPE OMXVideoDecoderBase::GetOutputColorFormat(int width, int height) {
